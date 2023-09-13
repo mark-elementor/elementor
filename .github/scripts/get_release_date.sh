5652f9c904ba2a2df9c0851546b0b7d435290c58
Mark Tyrin <markt@elementor.com>
2023-09-13 13:57:09 +0300

    Fix

--- .github/scripts/get_release_date.sh
+++ .github/scripts/get_release_date.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-RELEASE_DATE=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/elementor/elementor/releases/tags/$INPUT_RELEASE" | jq -r '.published_at')
+RESPONSE=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/elementor/elementor/releases/tags/$INPUT_RELEASE")
+
+echo "DEBUG: Full response from API: $RESPONSE" # For debugging
+
+RELEASE_DATE=$(echo "$RESPONSE" | jq -r '.published_at')
+
+echo "DEBUG: Extracted RELEASE_DATE: $RELEASE_DATE" # For debugging
+
 echo "RELEASE_DATE=$RELEASE_DATE" >> $GITHUB_ENV
