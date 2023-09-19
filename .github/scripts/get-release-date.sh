c3a2f52330006de9b0752f61a840924b71ee5165
Mark Tyrin <markt@elementor.com>
2023-09-19 09:38:41 +0300

    Output vars

---
+++ .github/scripts/get-release-date.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+RELEASE_DATE=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/elementor/elementor/releases/tags/$INPUT_RELEASE" | jq -r '.published_at')
+echo "name=RELEASE_DATE::$RELEASE_DATE"
